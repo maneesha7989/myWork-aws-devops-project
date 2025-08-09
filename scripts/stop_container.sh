0d9a9c1dd2265c9d35a6bce46172c5559ec65161
Maneesha AHS <maneesha.agnihotram.p@gmail.com>
2025-08-09 16:01:21 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -5,6 +5,7 @@ containerid=$(docker ps -q)
 # checking if the command output is not Empty
 if [ -n "$containerid" ]; then
   docker stop $containerid && docker rm -f $containerid
+  echo "container stopped and removed"
 else
   echo "no running containers to reomve"
 fi
